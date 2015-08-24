b785598f3160ae12e4323c501cc874cb9ecc440d
Thomas Prommer <thomas@socapro.com>
2015-08-24 05:37:09 -0700

    added empty pokal quote files lost in transition

--- tmsrc/cronjobs/btm/seasonchange/pokal/pokal_setup.pl
+++ tmsrc/cronjobs/btm/seasonchange/pokal/pokal_setup.pl
@@ -1,8 +1,8 @@
 require "dfb_runde1.pl";
 require "btm_pokal_ansetzung.pl";
-`mv tmi_pokal_leer.txt /tmdata/tmi/pokal/pokal_quote.txt`;
+`cp tmi_pokal_leer.txt /tmdata/tmi/pokal/pokal_quote.txt`;
 require "tmi_pokal_ansetzung.pl";
-`mv pokal_quote_btm.txt /tmdata/btm/pokal/pokal_quote.txt`;
+`cp pokal_quote_btm.txt /tmdata/btm/pokal/pokal_quote.txt`;
 
 print "\n\n BTM und TMI Pokal neu aufgesetzt !\n";
 
