d7a748bebd4af67a23aba2e440ff4b05c68383fb
Thomas Prommer <thomas@socapro.com>
2015-12-31 09:26:55 -0800

    added seasons for year 2016

--- tmsrc/cgi-mod/index.pl
+++ tmsrc/cgi-mod/index.pl
@@ -851,9 +851,10 @@ print '
 
 	<td bgcolor=white>
 
-
+<div style="max-width:300px">
 <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
 <!-- TM 728x90 -->
+
 <ins class="adsbygoogle"
      style="display:inline-block;width:728px;height:90px"
      data-ad-client="ca-pub-7303526190369900"
@@ -861,6 +862,7 @@ print '
 <script>
 (adsbygoogle = window.adsbygoogle || []).push({});
 </script>
+</div>
 <!--
 <iframe id=\'acae648c\' name=\'acae648c\' src=\'http://ads.socapro.com/www/delivery/afr.php?zoneid=83&amp;cb=INSERT_RANDOM_NUMBER_HERE&amp;ct0=INSERT_CLICKURL_HERE\' frameborder=\'0\' scrolling=\'no\' width=\'120\' height=\'600\'><a href=\'http://ads.socapro.com/www/delivery/ck.php?n=a3108930&amp;cb=INSERT_RANDOM_NUMBER_HERE\' target=\'_blank\'><img src=\'http://ads.socapro.com/www/delivery/avw.php?zoneid=83&amp;cb=INSERT_RANDOM_NUMBER_HERE&amp;n=a3108930&amp;ct0=INSERT_CLICKURL_HERE\' border=\'0\' alt=\'\' /></a></iframe>
 -->
