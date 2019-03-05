022fe2045db290ad16b6ac0848eecf8ea0c4da62
Thomas Prommer <thomasprommer@Thomas-Prommer-SG.local>
2019-03-05 07:51:29 -0800

    g

--- tmsrc/cgi-mod/index.pl
+++ tmsrc/cgi-mod/index.pl
@@ -381,7 +381,7 @@ print '
 <div align=center style="width:830px;text-align:center;padding-top:5px;padding-bottom:10px;color:black;background-color:#fc3;font-size:10px;">
 
 Augrund eines weitreichenden Datenverlusts mussten wir den Spielbetrieb und Datenstand leider auf die Saison 2018\'2 Spieltag 20 zuruecksetzen.<br/>
-Der "Daten Reset" betrifft auch Accounts, Passwoerter etc. Probleme bitte <a href="http://community.tipmaster.de/showthread.php?t=34487">hier melden</a>. Die Tippabgabe ist offen.
+Der "Daten Reset" betrifft auch Accounts, Passwoerter, Pokal und Eurocup etc. Probleme bitte <a href="http://community.tipmaster.de/showthread.php?t=34487">hier melden</a>. Die Tippabgabe ist offen.
 </div>.
 <table border=0 cellpadding=0 cellspacing=0><tr><td valign=top><tr><td valign=top>
 
