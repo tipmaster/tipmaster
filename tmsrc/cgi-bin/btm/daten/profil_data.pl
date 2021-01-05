44561f08776bfdc92e63325cb6ed4466491493cf
Thomas Prommer <thomasprommer@Thomas-Prommer-SG.local>
2021-01-05 06:58:40 -0800

    g

--- tmsrc/cgi-bin/btm/daten/profil_data.pl
+++ tmsrc/cgi-bin/btm/daten/profil_data.pl
@@ -18,113 +18,105 @@
 
 =cut
 
-use lib '/tmapp/tmsrc/cgi-bin/'; 
+use lib '/tmapp/tmsrc/cgi-bin/';
 use TMSession;
-my $session = TMSession::getSession(btm_login => 1);
+my $session = TMSession::getSession( btm_login => 1 );
 my $trainer = $session->getUser();
-my $leut = $trainer;
-
+my $leut    = $trainer;
 
 use CGI;
-$query = new CGI;
-$wohnort = $query->param('wohnort');
-$land = $query->param('land');
+$query      = new CGI;
+$wohnort    = $query->param('wohnort');
+$land       = $query->param('land');
 $bundesland = $query->param('bundesland');
-$beruf = $query->param('beruf');
-$liebling = $query->param('liebling');
-$hobby = $query->param('hobby');
-$motto = $query->param('motto');
-$gb1 = $query->param('gb1');
-$gb2 = $query->param('gb2');
-$gb3 = $query->param('gb3');
-$notifval = $query->param('notifier');
+$beruf      = $query->param('beruf');
+$liebling   = $query->param('liebling');
+$hobby      = $query->param('hobby');
+$motto      = $query->param('motto');
+$gb1        = $query->param('gb1');
+$gb2        = $query->param('gb2');
+$gb3        = $query->param('gb3');
+$notifval   = $query->param('notifier');
 $ecnotifval = $query->param('ecnotifier');
 
-if ( $gb1 <10 ) {$ca = 0 }
-if ( $gb2 <10 ) {$cb = 0 }
+if ( $gb1 < 10 ) { $ca = 0 }
+if ( $gb2 < 10 ) { $cb = 0 }
 
-$cc = "." ;
+$cc = ".";
 
-$geburtstag = $ca . $gb1 .$cc. $cb . $gb2 .$cc.$gb3 ;
+$geburtstag = $ca . $gb1 . $cc . $cb . $gb2 . $cc . $gb3;
 
+$aa    = "&";
+$ae    = "!";
+$suche = $ae . $aa . $trainer . $aa;
+$r     = 0;
 
+$zeilen = $suche . $wohnort . $aa . $land . $aa . $geburtstag . $aa . $bundesland . $aa . $beruf . $aa . $liebling . $aa . $hobby . $aa . $motto . $aa;
 
-
-
-$aa = "&" ;
-$ae = "!" ;
-$suche = $ae . $aa . $trainer . $aa ;
-$r = 0;
-
-
-$zeilen = $suche . $wohnort . $aa . $land . $aa . $geburtstag . $aa . $bundesland . $aa . $beruf . $aa . $liebling . $aa . $hobby . $aa . $motto . $aa ;
-
-if ($zeilen=~/javascript/) {exit;} # Blacklisting ist kein effektiver Schutz! Besser erst gar nicht mit sowas anfangen.
+if ( $zeilen =~ /javascript/ ) { exit; }    # Blacklisting ist kein effektiver Schutz! Besser erst gar nicht mit sowas anfangen.
 
 $rof = 0;
+
 # Ah, ja. Whitelisting! Viel besser. Aber es muss Alles geprüft werden, auch die Felder, die sich aus Comboboxen, Radios oder Checkboxen speisen. Auch das Geb.datum mit Felder von Länge 2 kann böse sein!
-if ( $wohnort =~ /[^A-Za-z_0-9\.\-,;! ]/) { $rof = 1 }
-if ( $land =~ /[^A-Za-z_0-9\.\-,;! ]/) { $rof = 1 }
-if ( $geburtstag =~ /[^A-Za-z_0-9\.\-,;! ]/) { $rof = 1 }
-if ( $bundesland =~ /[^A-Za-z_0-9\.\-,;! ]/) { $rof = 1 }
-if ( $beruf =~ /[^A-Za-z_0-9\.\-,;! ]/) { $rof = 1 }
-if ( $liebling =~ /[^A-Za-z_0-9\.\-,;! ]/) { $rof = 1 }
-if ( $hobby =~ /[^A-Za-z_0-9\.\-,;! ]/) { $rof = 1 }
-if ( $motto =~ /[^A-Za-z_0-9\.\-,;! ]/) { $rof = 1 }
+if ( $wohnort =~ /[^A-Za-z_0-9\.\-,;! ]/ )    { $rof = 1 }
+if ( $land =~ /[^A-Za-z_0-9\.\-,;! ]/ )       { $rof = 1 }
+if ( $geburtstag =~ /[^A-Za-z_0-9\.\-,;! ]/ ) { $rof = 1 }
+if ( $bundesland =~ /[^A-Za-z_0-9\.\-,;! ]/ ) { $rof = 1 }
+if ( $beruf =~ /[^A-Za-z_0-9\.\-,;! ]/ )      { $rof = 1 }
+if ( $liebling =~ /[^A-Za-z_0-9\.\-,;! ]/ )   { $rof = 1 }
+if ( $hobby =~ /[^A-Za-z_0-9\.\-,;! ]/ )      { $rof = 1 }
+if ( $motto =~ /[^A-Za-z_0-9\.\-,;! ]/ )      { $rof = 1 }
 
 if ( $rof == 1 ) {
-print "Content-type: text/html\n\n";
-print "<body bgcolor=#eeeeee text=black><font face=verdana size=1>";
-print "Bundesliga - TipMaster<br><br>Eine ihrer Angabe enthaelt ein nicht gueltiges Zeichen ...<br>Bitte verzichten sie auf Umlaute sowie auf Sonderzeichen !<br>";
-exit ;
+	print "Content-type: text/html\n\n";
+	print "<body bgcolor=#eeeeee text=black><font face=verdana size=1>";
+	print "Bundesliga - TipMaster<br><br>Eine ihrer Angabe enthaelt ein nicht gueltiges Zeichen ...<br>Bitte verzichten sie auf Umlaute sowie auf Sonderzeichen !<br>";
+	exit;
 }
 
 if ( $trainer ne "Gast Zugang" ) {
 
-$datei = "/tmdata/btm/db/profile/" . $trainer . ".txt" ;
-open(D8,">$datei");
-flock (D8 , 2);
-print D8 "$zeilen\n";
-flock (D8 , 8);
-close (D8) ;
+	$datei = "/tmdata/btm/db/profile/" . $trainer . ".txt";
+	open( D8, ">$datei" );
+	flock( D8, 2 );
+	print D8 "$zeilen\n";
+	flock( D8, 8 );
+	close(D8);
 
-#if (1) {
+	#if (1) {
 ### setzen von flag beim notifier
-#my $flags = O_RDWR;
-#my $notifierentry = 0;
-#if ($notifval eq "formular") {
-#	$notifierentry = 1;
-#}
-#my $mode = "0777";
-#my $db = tie %notifiers, 'DB_File', "/tmdata/btm/notifiers.dbm", $flags, $mode, $DB_HASH or print "Cannot create DB: $!";
-#
-#my $fd = $db->fd;                                            # Get file descriptor
-#open DBM, "+<&=$fd" or die "Could not dup DBM for lock: $!"; # Get dup filehandle
-#flock DBM, LOCK_EX;                                          # Lock exclusively
-#undef $db;                                                   # Avoid untie probs
-#$notifiers{"$trainer"} = $notifierentry;
-#untie %notifiers;
-#flock DBM, LOCK_UN;
-#
-#
-#if (1) {
-#my $ecnotifierentry = 1;
-#if ($ecnotifval eq "ecformular") {
-#        $ecnotifierentry = 0;
-#}
-#my $ecdb = tie %ecnotifiers, 'DB_File', "/tmdata/cl/notifiers.dbm", $flags, $mode, $DB_HASH or print "Cannot create DB: $!";
-#
-#my $fd = $ecdb->fd;                                            # Get file descriptor
-#open DBZ, "+<&=$fd" or die "Could not dup DBM for lock: $!"; # Get dup filehandle
-#flock DBZ, LOCK_EX;                                          # Lock exclusively
-#undef $ecdb;                                                   # Avoid untie probs
-#$ecnotifiers{"$trainer"} = $ecnotifierentry;
-#untie %ecnotifiers;
-#flock DBZ, LOCK_UN;
-#}
-
-}
-
+	#my $flags = O_RDWR;
+	#my $notifierentry = 0;
+	#if ($notifval eq "formular") {
+	#	$notifierentry = 1;
+	#}
+	#my $mode = "0777";
+	#my $db = tie %notifiers, 'DB_File', "/tmdata/btm/notifiers.dbm", $flags, $mode, $DB_HASH or print "Cannot create DB: $!";
+	#
+	#my $fd = $db->fd;                                            # Get file descriptor
+	#open DBM, "+<&=$fd" or die "Could not dup DBM for lock: $!"; # Get dup filehandle
+	#flock DBM, LOCK_EX;                                          # Lock exclusively
+	#undef $db;                                                   # Avoid untie probs
+	#$notifiers{"$trainer"} = $notifierentry;
+	#untie %notifiers;
+	#flock DBM, LOCK_UN;
+	#
+	#
+	#if (1) {
+	#my $ecnotifierentry = 1;
+	#if ($ecnotifval eq "ecformular") {
+	#        $ecnotifierentry = 0;
+	#}
+	#my $ecdb = tie %ecnotifiers, 'DB_File', "/tmdata/cl/notifiers.dbm", $flags, $mode, $DB_HASH or print "Cannot create DB: $!";
+	#
+	#my $fd = $ecdb->fd;                                            # Get file descriptor
+	#open DBZ, "+<&=$fd" or die "Could not dup DBM for lock: $!"; # Get dup filehandle
+	#flock DBZ, LOCK_EX;                                          # Lock exclusively
+	#undef $ecdb;                                                   # Avoid untie probs
+	#$ecnotifiers{"$trainer"} = $ecnotifierentry;
+	#untie %ecnotifiers;
+	#flock DBZ, LOCK_UN;
+	#}
 
 }
 
@@ -133,12 +125,11 @@ print "<body bgcolor=#eeeeee text=black><font face=verdana size=1>";
 print "Bundesliga - TipMaster<br><br>Ihr Trainer - Profil ist aktualisiert ...<br>Sie werden weitergeleitet ...";
 print "<form name=Testform action=/cgi-mod/btm/login.pl method=post><input type=hidden name=trainer value=\"$trainer\"><input type=hidden name=password value=\"$pass\"></form>";
 print "<script language=JavaScript>\n";
-print"   function AbGehts()\n";
-print"   {\n";
-print"    document.Testform.submit();\n";
-print"    }\n";
-print"   window.setTimeout(\"AbGehts()\",100);\n";
-print"  </script>\n";
+print "   function AbGehts()\n";
+print "   {\n";
+print "    document.Testform.submit();\n";
+print "    }\n";
+print "   window.setTimeout(\"AbGehts()\",100);\n";
+print "  </script>\n";
 
-
-exit ;
+exit;
--- tmsrc/cgi-bin/tmi/daten/profil_data.pl
+++ tmsrc/cgi-bin/tmi/daten/profil_data.pl
@@ -18,69 +18,65 @@
 
 =cut
 
-use lib '/tmapp/tmsrc/cgi-bin/'; 
+use lib '/tmapp/tmsrc/cgi-bin/';
 use TMSession;
-my $session = TMSession::getSession(tmi_login => 1);
+my $session = TMSession::getSession( tmi_login => 1 );
 my $trainer = $session->getUser();
-my $leut = $trainer;
+my $leut    = $trainer;
 
 use CGI;
-$query = new CGI;
-$wohnort = $query->param('wohnort');
-$land = $query->param('land');
+$query      = new CGI;
+$wohnort    = $query->param('wohnort');
+$land       = $query->param('land');
 $bundesland = $query->param('bundesland');
-$beruf = $query->param('beruf');
-$liebling = $query->param('liebling');
-$hobby = $query->param('hobby');
-$motto = $query->param('motto');
-$gb1 = $query->param('gb1');
-$gb2 = $query->param('gb2');
-$gb3 = $query->param('gb3');
-$notifval = $query->param('notifier');
+$beruf      = $query->param('beruf');
+$liebling   = $query->param('liebling');
+$hobby      = $query->param('hobby');
+$motto      = $query->param('motto');
+$gb1        = $query->param('gb1');
+$gb2        = $query->param('gb2');
+$gb3        = $query->param('gb3');
+$notifval   = $query->param('notifier');
 
-if ( $gb1 <10 ) {$ca = 0 }
-if ( $gb2 <10 ) {$cb = 0 }
+if ( $gb1 < 10 ) { $ca = 0 }
+if ( $gb2 < 10 ) { $cb = 0 }
 
-$cc = "." ;
+$cc = ".";
 
-$geburtstag = $ca . $gb1 .$cc. $cb . $gb2 .$cc.$gb3 ;
+$geburtstag = $ca . $gb1 . $cc . $cb . $gb2 . $cc . $gb3;
 
+$aa    = "&";
+$ae    = "!";
+$suche = $ae . $aa . $trainer . $aa;
+$r     = 0;
 
+$zeilen = $suche . $wohnort . $aa . $land . $aa . $geburtstag . $aa . $bundesland . $aa . $beruf . $aa . $liebling . $aa . $hobby . $aa . $motto . $aa;
 
-$aa = "&" ;
-$ae = "!" ;
-$suche = $ae . $aa . $trainer . $aa ;
-$r = 0;
-
-
-$zeilen = $suche . $wohnort . $aa . $land . $aa . $geburtstag . $aa . $bundesland . $aa . $beruf . $aa . $liebling . $aa . $hobby . $aa . $motto . $aa ;
-
-if ($zeilen=~/javascript/) {exit;} # Netter Versuch
+if ( $zeilen =~ /javascript/ ) { exit; }    # Netter Versuch
 
 $rof = 0;
-if ( $wohnort =~ /[^A-Za-z_0-9\.\-,;! ]/) { $rof = 1 }
-if ( $land =~ /[^A-Za-z_0-9\.\-,;! ]/) { $rof = 1 }
-if ( $geburtstag =~ /[^A-Za-z_0-9\.\-,;! ]/) { $rof = 1 }
-if ( $bundesland =~ /[^A-Za-z_0-9\.\-,;! ]/) { $rof = 1 }
-if ( $beruf =~ /[^A-Za-z_0-9\.\-,;! ]/) { $rof = 1 }
-if ( $liebling =~ /[^A-Za-z_0-9\.\-,;! ]/) { $rof = 1 }
-if ( $hobby =~ /[^A-Za-z_0-9\.\-,;! ]/) { $rof = 1 }
-if ( $motto =~ /[^A-Za-z_0-9\.\-,;! ]/) { $rof = 1 }
+if ( $wohnort =~ /[^A-Za-z_0-9\.\-,;! ]/ )    { $rof = 1 }
+if ( $land =~ /[^A-Za-z_0-9\.\-,;! ]/ )       { $rof = 1 }
+if ( $geburtstag =~ /[^A-Za-z_0-9\.\-,;! ]/ ) { $rof = 1 }
+if ( $bundesland =~ /[^A-Za-z_0-9\.\-,;! ]/ ) { $rof = 1 }
+if ( $beruf =~ /[^A-Za-z_0-9\.\-,;! ]/ )      { $rof = 1 }
+if ( $liebling =~ /[^A-Za-z_0-9\.\-,;! ]/ )   { $rof = 1 }
+if ( $hobby =~ /[^A-Za-z_0-9\.\-,;! ]/ )      { $rof = 1 }
+if ( $motto =~ /[^A-Za-z_0-9\.\-,;! ]/ )      { $rof = 1 }
 
 if ( $rof == 1 ) {
-print "Content-type: text/html\n\n";
-print "<body bgcolor=#eeeeee text=black><font face=verdana size=1>";
-print "TipMaster international<br><br>Eine ihrer Angabe enthaelt ein nicht gueltiges Zeichen ...<br>Bitte verzichten sie auf Umlaute sowie auf Sonderzeichen !<br>";
-exit ;
+	print "Content-type: text/html\n\n";
+	print "<body bgcolor=#eeeeee text=black><font face=verdana size=1>";
+	print "TipMaster international<br><br>Eine ihrer Angabe enthaelt ein nicht gueltiges Zeichen ...<br>Bitte verzichten sie auf Umlaute sowie auf Sonderzeichen !<br>";
+	exit;
 }
 
-$datei = "/tmdata/btm/db/profile/" . $trainer . ".txt" ;
-open(D8,">$datei");
-flock (D8 , 2);
+$datei = "/tmdata/btm/db/profile/" . $trainer . ".txt";
+open( D8, ">$datei" );
+flock( D8, 2 );
 print D8 "$zeilen\n";
-flock (D8 , 8);
-close (D8) ;
-
+flock( D8, 8 );
+close(D8);
 
 #if (1) {
 ## setzen von flag beim notifier
@@ -101,23 +97,17 @@ close (D8) ;
 #untie %notifiers;
 #}
 
-
-
 print "Content-type: text/html\n\n";
 print "<body bgcolor=#eeeeee text=black><font face=verdana size=1>";
 print "TipMaster international<br><br>Ihr Trainer - Profil ist aktualisiert ...<br>Sie werden weitergeleitet ...";
 print "<form name=Testform action=/cgi-mod/tmi/login.pl method=post></form>";
 print "<script language=JavaScript>\n";
-print"   function AbGehts()\n";
-print"   {\n";
-print"    document.Testform.submit();\n";
-print"    }\n";
-print"   window.setTimeout(\"AbGehts()\",100);\n";
-print"  </script>\n";
+print "   function AbGehts()\n";
+print "   {\n";
+print "    document.Testform.submit();\n";
+print "    }\n";
+print "   window.setTimeout(\"AbGehts()\",100);\n";
+print "  </script>\n";
 
-
-exit ;
-
-
-
+exit;
 
