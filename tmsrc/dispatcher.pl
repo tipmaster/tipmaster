4b09228705851b9022880591dc33de796858c2d3
Thomas Prommer <thomasprommer@Thomas-Prommer-SG.local>
2021-03-11 04:54:54 -0800

    g

---
+++ tmsrc/dispatcher.pl
@@ -0,0 +1,15 @@
+#!/opt/ActivePerl-5.24/bin/perl
+
+package Dispatcher;
+use lib '/livescore/trunk/res/www/cgi-bin/';
+use lib '/livescore/trunk/res/www/cgi-bin/lib';
+
+while ( my $q = new CGI::Fast ) {
+
+	use Data::Dumper;
+	Global::Logger::debug("main::dispatcher_tm.pl");
+	Global::Logger::debug( Dumper($q) );
+
+}
+
+1;
