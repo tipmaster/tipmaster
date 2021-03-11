89725fe1441b71181f8625ca98954542653645fd
Thomas Prommer <thomasprommer@Thomas-Prommer-SG.local>
2021-03-11 07:03:15 -0800

    auto commit

--- tmsrc/cgi-bin/Test.pm
+++ tmsrc/cgi-bin/Test.pm
@@ -1155,6 +1155,7 @@ sub banner_gross {
 #<iframe id=\'acae648c\' name=\'acae648c\' src=\'http://advertising.fussball-liveticker.eu/www/delivery/afr.php?zoneid=84&amp;cb=INSERT_RANDOM_NUMBER_HERE&amp;ct0=INSERT_CLICKURL_HERE\' frameborder=\'0\' scrolling=\'no\' width=\'468\' height=\'60\'><a href=\'http://advertising.fussball-liveticker.eu/www/delivery/ck.php?n=a3108930&amp;cb=INSERT_RANDOM_NUMBER_HERE\' target=\'_blank\'><img src=\'http://advertising.fussball-liveticker.eu/www/delivery/avw.php?zoneid=84&amp;cb=INSERT_RANDOM_NUMBER_HERE&amp;n=a3108930&amp;ct0=INSERT_CLICKURL_HERE\' border=\'0\' alt=\'\' /></a></iframe>
 #-->
 
+return '';
 
 $number = '
 <!-- Paste the above code right above the closing </head> of the HTML in your tipmaster.de -->
