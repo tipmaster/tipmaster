049ff72f002acb78c25e1ddc65362d1af9ec8e54
Thomas Prommer <thomasprommer@Thomas-Prommer-SG.local>
2021-03-11 05:06:33 -0800

    auto commit

--- tmsrc/dispatcher.pl
+++ tmsrc/dispatcher.pl
@@ -3,13 +3,13 @@
 package Dispatcher;
 use lib '/livescore/trunk/res/www/cgi-bin/';
 use lib '/livescore/trunk/res/www/cgi-bin/lib';
+use CGI::Fast ( -utf8 );
+use utf8;
 
 while ( my $q = new CGI::Fast ) {
-
 	use Data::Dumper;
 	Global::Logger::debug("main::dispatcher_tm.pl");
 	Global::Logger::debug( Dumper($q) );
-
 }
 
 1;
