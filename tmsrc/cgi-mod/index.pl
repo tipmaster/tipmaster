596e103aa7c80b3c10e830096dd029c4e4ac2dc0
Thomas Prommer <thomasprommer@Thomas-Prommer-SG.local>
2021-03-13 12:37:01 -0800

    auto commit

--- tmsrc/cgi-mod/index.pl
+++ tmsrc/cgi-mod/index.pl
@@ -850,7 +850,9 @@ print '
 
 
 <!-- tipmaster.de_300x600: Begin -->
+<!--
 <div class="adsbyvli" data-ad-slot="vi_1335352009"></div><script>(vitag.Init = window.vitag.Init || []).push(function(){viAPItag.display("vi_1335352009")})</script>
+-->
 <!-- tipmaster.de_300x600 End -->
 
 </div>
