6adf89ddcfd8ee009840ceb25c8ab3e6dac45c5b
Tom <thomas.prommer@gmail.com>
2021-09-09 17:55:41 -0700

    auto commit

--- tmsrc/cgi-bin/tag_bottom.pl
+++ tmsrc/cgi-bin/tag_bottom.pl
@@ -31,15 +31,15 @@ print "
 <script>
   window.googletag = window.googletag || {cmd: []};
   googletag.cmd.push(function() {
- googletag.defineSlot('/22495599872/DE-TM-970x250-SUBPAGE', [970, 250], 'div-gpt-ad-1631234964627-0').addService(googletag.pubads());
+  googletag.defineSlot('/22495599872/DE-TM-970x250-SUBPAGE-BOTTOM', [970, 250], 'div-gpt-ad-1631235278221-0').addService(googletag.pubads());
     googletag.pubads().enableSingleRequest();
     googletag.enableServices();
   });
 </script>
-<!-- /22495599872/DE-TM-970x250-SUBPAGE -->
-<div id='div-gpt-ad-1631234964627-0' style='min-width: 970px; min-height: 250px;'>
+<!-- /22495599872/DE-TM-970x250-SUBPAGE-BOTTOM -->
+<div id='div-gpt-ad-1631235278221-0' style='min-width: 970px; min-height: 250px;'>
   <script>
-    googletag.cmd.push(function() { googletag.display('div-gpt-ad-1631234964627-0'); });
+    googletag.cmd.push(function() { googletag.display('div-gpt-ad-1631235278221-0'); });
   </script>
 </div>
 ";
