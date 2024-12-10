ded2b984b38c3f6c52e77075a9e625a3b70d3afb
TFWG <accounts@flywheel.bz>
2024-12-10 09:32:45 +0900

    g

--- tmsrc/cgi-mod/btm/tips.pl
+++ tmsrc/cgi-mod/btm/tips.pl
@@ -539,7 +539,67 @@ new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
 })(window,document,'script','dataLayer','GTM-53T3SLD');</script>
-<!-- End Google Tag Manager -->";
+<!-- End Google Tag Manager -->
+<!-- POSTHOG START -->
+<script>
+    !(function (t, e) {
+      var o, n, p, r;
+      e.__SV ||
+        ((window.posthog = e),
+        (e._i = []),
+        (e.init = function (i, s, a) {
+          function g(t, e) {
+            var o = e.split(\".\");
+            2 == o.length && ((t = t[o[0]]), (e = o[1])),
+              (t[e] = function () {
+                t.push([e].concat(Array.prototype.slice.call(arguments, 0)));
+              });
+          }
+          ((p = t.createElement(\"script\")).type = \"text/javascript\"),
+            (p.async = !0),
+            (p.src =
+              s.api_host.replace(\".i.posthog.com\", \"-assets.i.posthog.com\") +
+              \"/static/array.js\"),
+            (r = t.getElementsByTagName(\"script\")[0]).parentNode.insertBefore(
+              p,
+              r
+            );
+          var u = e;
+          for (
+            void 0 !== a ? (u = e[a] = []) : (a = \"posthog\"),
+              u.people = u.people || [],
+              u.toString = function (t) {
+                var e = \"posthog\";
+                return (
+                  \"posthog\" !== a && (e += \".\" + a), t || (e += \" (stub)\"), e
+                );
+              },
+              u.people.toString = function () {
+                return u.toString(1) + \".people (stub)\";
+              },
+              o =
+                \"capture identify alias people.set people.set_once set_config register register_once unregister opt_out_capturing has_opted_out_capturing opt_in_capturing reset isFeatureEnabled onFeatureFlags getFeatureFlag getFeatureFlagPayload reloadFeatureFlags group updateEarlyAccessFeatureEnrollment getEarlyAccessFeatures getActiveMatchingSurveys getSurveys getNextSurveyStep onSessionId setPersonProperties\".split(
+                  \" \"
+                ),
+              n = 0;
+            n < o.length;
+            n++
+          )
+            g(u, o[n]);
+          e._i.push([i, s, a]);
+        }),
+        (e.__SV = 1));
+    })(document, window.posthog || []);
+    posthog.init(\"phc_iJKj0EowViIqaA6qV2nD6gv8sDIN4DcflJOIWGEQROj\", {
+      api_host: \"https://us.i.posthog.com\",
+      person_profiles: \"always\",
+      autocapture: {
+        dom_event_allowlist: [\"click\"]
+      },
+    });
+</script>
+<!-- POSTHOG END -->
+";
 print '<br>'.$banner_bottom;
 exit;
 
