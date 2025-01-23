68e58003a3ed1f2f850d7230a08dcb23ce856928
TFWG <accounts@flywheel.bz>
2025-01-23 17:30:21 +0100

    g

--- tmsrc/cgi-mod/index.pl
+++ tmsrc/cgi-mod/index.pl
@@ -913,7 +913,7 @@ print '
 						</tr></table> 
 						
 						</td><td class=impressum align=center>
-						&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;  Copyright 1998-2024 - The Flywheel</b>
+						&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;  Copyright 1998-2025 - The Flywheel</b>
 			
 			
 						
